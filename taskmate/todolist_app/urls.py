1082860711bc7986c977e4ffd819165cd070d12e
Jose Sosa <josephsosa0777@gmail.com>
2021-05-03 16:49:11 -0500

    Editing Queryset item

--- taskmate/todolist_app/urls.py
+++ taskmate/todolist_app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # set the name for the view and for the link:
     path('', views.todolist, name='todolist'),
     path('delete/<task_id>', views.delete_task, name='delete_task'),
-
+    path('edit/<task_id>', views.edit_task, name='edit_task'),
     path('contact', views.contact, name='contact'),
     path('about', views.about, name='about'),
 ]
