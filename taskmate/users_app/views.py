93ebd99cf57a9e07d20180fdf454b15948bab6fa
Jose Sosa <josephsosa0777@gmail.com>
2021-05-03 22:56:04 -0500

    register views part1

--- taskmate/users_app/views.py
+++ taskmate/users_app/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def register(request):
     # return HttpResponse("users_app working!")
-    register_form = UserCreationForm()
+    if request.method == "POST":
+        register_form = UserCreationForm(request.POST)
+        if register_form.is_valid():
+            # register_form.save()
+            messages.success(
+                request, ("New User Account Created, Loin to Get Started!"))
+            return redirect('todolist')
+    else:
+        register_form = UserCreationForm()
     return render(request, 'register.html', {'register_form': register_form})
