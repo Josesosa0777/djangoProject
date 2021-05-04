29f41b8c8262579d48ad737713311b8b184da8ff
Jose Sosa <josephsosa0777@gmail.com>
2021-05-04 00:33:22 -0500

    Task of Logged-in User Only

--- taskmate/todolist_app/views.py
+++ taskmate/todolist_app/views.py
@@ -4,12 +4,14 @@ from todolist_app.models import TaskList
 from todolist_app.forms import TaskForm
 from django.contrib import messages
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 # # Create your views here.
 # def todolist(request):
 #     return HttpResponse("Welcome to Task page")
 
 
+@login_required
 def todolist(request):
     if request.method == "POST":
         form = TaskForm(request.POST or None)
@@ -18,19 +20,21 @@ def todolist(request):
         messages.success(request, ("New Task Added!"))
         return redirect('todolist')
     else:
-        all_tasks = TaskList.objects.all()
+        all_tasks = TaskList.objects.filter(manage=request.user)
         paginator = Paginator(all_tasks, 5)  # number of items per page
         page = request.GET.get('pg')
         all_tasks = paginator.get_page(page)
         return render(request, 'todolist.html', {'all_tasks': all_tasks})
 
 
+@login_required
 def delete_task(request, task_id):
     task = TaskList.objects.get(pk=task_id)  # pk is for primary key
     task.delete()
     return redirect('todolist')
 
 
+@login_required
 def edit_task(request, task_id):
     if request.method == "POST":
         task = TaskList.objects.get(pk=task_id)
@@ -44,6 +48,7 @@ def edit_task(request, task_id):
         return render(request, 'edit.html', {'task_obj': task_obj})
 
 
+@login_required
 def complete_task(request, task_id):
     task = TaskList.objects.get(pk=task_id)  # pk is for primary key
     task.done = True
@@ -51,6 +56,7 @@ def complete_task(request, task_id):
     return redirect('todolist')
 
 
+@login_required
 def pending_task(request, task_id):
     task = TaskList.objects.get(pk=task_id)  # pk is for primary key
     task.done = False
