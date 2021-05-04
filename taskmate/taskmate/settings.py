463f196e1fea3f25a3a0b2de81f2a00f4ff894fd
Josesosa0777 <josephsosa0777@gmail.com>
2021-05-04 13:27:51 -0500

    adding psycopg2

--- taskmate/taskmate/settings.py
+++ taskmate/taskmate/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 import environ
@@ -123,7 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
@@ -136,3 +136,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'  # the version that I have
 
 LOGIN_REDIRECT_URL = 'todolist'
 LOGIN_URL = 'login'
+
+django_heroku.settings(locals())
