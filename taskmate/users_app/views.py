779cd6181759f4bee2a6b76779b190497193ca83
Jose Sosa <josephsosa0777@gmail.com>
2021-05-03 22:39:56 -0500

    register template

--- taskmate/users_app/views.py
+++ taskmate/users_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def register(request):
+    # return HttpResponse("users_app working!")
+    register_form = UserCreationForm()
+    return render(request, 'register.html', {'register_form': register_form})
